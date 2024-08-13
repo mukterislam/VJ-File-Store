e52f04c9352b6075c31df992b456f3145adf27f9
mukterislam <70107450+mukterislam@users.noreply.github.com>
2024-08-14 01:32:41 +0600

    Update config.py

--- config.py
+++ config.py
@@ -25,10 +25,10 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5711744886').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "ddbossfiles_bot") # without @
-PORT = environ.get("PORT", "8080")
+PORT = environ.get("PORT", "10000")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
@@ -39,7 +39,7 @@ DB_URI = environ.get("DB_URI", "mongodb+srv://mdmukterislam:GvNYWPZ6jjBxpohT@clu
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
