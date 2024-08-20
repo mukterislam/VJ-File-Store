220ee08a8d46f739e6dae75a8d31738ed39cfbe2
mukterislam <70107450+mukterislam@users.noreply.github.com>
2024-08-21 01:34:58 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Subnvjvscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 
